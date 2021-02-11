259467c4dc83a2663c6173871c3ae4b3cef7f5c6
Sean LaFlam <sean.laflam@gmail.com>
2021-02-10 23:03:49 -0500

    Standard Binary Search Complete

--- binary_search_project/lib/binary_search.js
+++ binary_search_project/lib/binary_search.js
@@ -1,9 +1,24 @@
 function binarySearch(array, target) {
+    console.log(array, target)
+    if (array.length === 0) return false
 
+    let midIdx = Math.floor(array.length/2)
+    let left = array.slice(0,midIdx)
+    let right = array.slice(midIdx+1)
+
+    if (target > array[midIdx]){
+        return binarySearch(right, target)
+    } else if (target < array[midIdx]){
+        return binarySearch(left, target)
+    } else {
+        return true
+    }
 }
 
 function binarySearchIndex(array, target) {
+    if (array.length === 0) return false
 
+    let midIdx = Math.floor
 }
 
 
@@ -11,3 +26,4 @@ module.exports = {
     binarySearch,
     binarySearchIndex
 };
+
