22af8d9b4f89cb410caf54a03e9e84f8f5f95f14
Sean LaFlam <sean.laflam@gmail.com>
2021-02-10 22:08:17 -0500

    Quicksort complete

--- quick_sort_project/lib/quick_sort.js
+++ quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,13 @@
 function quickSort(array) {
+    if (array.length <= 1) return array
 
+    let pivot = array.shift()
+    let left = array.filter(ele => ele < pivot)
+    let right = array.filter(ele => ele >= pivot)
+
+    let sortedLeft = quickSort(left)
+    let sortedRight = quickSort(right)
+    return [...sortedLeft, pivot, ...sortedRight]
 }
 
 
