616ccd152b13cb3f4f45c0067b51cd9770707881
Sean LaFlam <sean.laflam@gmail.com>
2021-02-12 00:39:32 -0500

    Update

---
+++ linked_list_project/lib/linked_list.js
@@ -0,0 +1,108 @@
+// ============================================================================
+// Implementation Exercise: Singly Linked List
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement a Singly Linked List and all of its methods below!
+//
+// ------------
+// Constraints:
+// ------------
+//
+// Make sure the time and space complexity of each is equivalent to those 
+// in the table provided in the Time and Space Complexity Analysis section
+// of your Linked List reading!
+//
+// -----------
+// Let's Code!
+// -----------
+
+// TODO: Implement a Linked List Node class here
+class Node {
+    constructor(val) {
+        this.next = null
+        this.value = val
+    }
+
+}
+
+// TODO: Implement a Singly Linked List class here
+class LinkedList {
+    constructor(val) {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    // TODO: Implement the addToTail method here
+    addToTail(val) {
+        const newNode = new Node(val)
+
+        if (!this.head){
+            this.head = newNode
+        } else {
+            this.tail.next = newNode
+        }
+
+        this.tail = newNode
+        this.length ++
+        return this
+    }
+
+    // TODO: Implement the removeTail method here
+    removeTail() {
+        if (!this.head) return undefined
+        let current = this.head
+        let newTail = current
+        while (current.next){
+            newTail = current
+            current = current.next
+        }
+    }
+
+    // TODO: Implement the addToHead method here
+    addToHead(val) {
+
+    }
+
+    // TODO: Implement the removeHead method here
+    removeHead() {
+
+    }
+
+    // TODO: Implement the contains method here
+    contains(target) {
+
+    }
+
+    // TODO: Implement the get method here
+    get(index) {
+
+    }
+
+    // TODO: Implement the set method here
+    set(index, val) {
+
+    }
+
+    // TODO: Implement the insert method here
+    insert(index, val) {
+
+    }
+
+    // TODO: Implement the remove method here
+    remove(index) {
+
+    }
+
+    // TODO: Implement the size method here
+    size() {
+
+    }
+}
+
+exports.Node = Node;
+exports.LinkedList = LinkedList;
