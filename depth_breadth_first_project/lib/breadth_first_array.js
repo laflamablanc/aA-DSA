c95a1aec1564e54ee63dc6c0bf815113c73c2566
Sean LaFlam <sean.laflam@gmail.com>
2021-02-12 22:00:25 -0500

    DF & BF Search

---
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -0,0 +1,20 @@
+function breadthFirstArray(root) {
+    let queue = [root]
+    let array = []
+
+    while (queue.length){
+        let node = queue.shift()
+
+        array.push(node.val)
+        if (node.left) queue.push(node.left)
+        if (node.right) queue.push(node.right)
+    }
+
+
+    return array
+
+}
+
+module.exports = {
+    breadthFirstArray
+};
